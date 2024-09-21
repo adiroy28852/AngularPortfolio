cfb0133ad44b98f5a339ee4bff88829f99850f19
Aditya Roy Chowdhury <98531857+adiroy28852@users.noreply.github.com>
2024-09-21 15:57:57 +0530

    Add files via upload

---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.css',
+  providers: []
+})
+export class PortfolioComponent {
+
+
+}
